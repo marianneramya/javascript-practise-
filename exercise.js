c9ec99ee1bc0e96c9479b28bdd6ceff79e6eb6bb
marianneramya <marianneramya2007@gmail.com>
2026-05-27 20:25:24 +0530

    practise

---
+++ exercise.js
@@ -0,0 +1,38 @@
+// // FIZZBUZZ EXERCISE 2
+// let num = Number(prompt("enter the number"));
+// if (num % 3 == 0 && num % 5 == 0) {
+//   console.log("Fizzbuzz");
+// } else if (num % 3 == 0) {
+//   console.log("Fizz");
+// } else if (num % 5 == 0) {
+//   console.log("Buzz");
+// } else {
+//   console.log(num);
+// }
+
+// //  ROLLERCOASTER RIDE TEST EXERCISE 3
+// let height = Number(prompt("enter your height"));
+// let medical_condition = String(
+//   prompt(
+//     " Does the person have any Vascular or Diabetic or Heart related problems (yes/no)",
+//   ),
+// );
+// let phobia = String(prompt("Does the person have claustrophobia(yes/no)"));
+// if (height >= 155 && medical_condition == "no") {
+//   console.log("you are eligible");
+// } else if (phobia == "no" && medical_condition == "no" && height >= 140) {
+//   console.log("you are eligible");
+// } else {
+//   console.log("you are not eligible");
+// }
+
+// FUNCTION
+// function CalcDiscount(Price, DiscountPercentage) {
+//   return Price * (DiscountPercentage / 100);
+// }
+// console.log(CalcDiscount(1345, 10));
+
+// // ARROW FUNCTION
+// const CalcDiscount = (Price, DiscountPercentage) =>
+//   Price * (DiscountPercentage / 100);
+// console.log(CalcDiscount(1345, 10));
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("hello World");
+var personName = prompt("what is your name");
+var personCity = prompt("enter your city");
+console.log();
